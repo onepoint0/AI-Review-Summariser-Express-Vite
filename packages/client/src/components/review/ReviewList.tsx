fec7ba10040ffa19807a56ed2b943444c0783b4f
Clare <clareivers@gmail.com>
2025-10-17 08:42:52 +1100

    refactor summarise mutation and review query

--- packages/client/src/components/review/ReviewList.tsx
+++ packages/client/src/components/review/ReviewList.tsx
@@ -1,10 +1,8 @@
 import axios from 'axios';
 import StarRating from './StarRating';
-import Skeleton from 'react-loading-skeleton';
 import { useMutation, useQuery } from '@tanstack/react-query';
 import { Button } from '../ui/button';
 import { HiSparkles } from 'react-icons/hi2';
-import { useState } from 'react';
 import ReviewSkeleton from './ReviewSkeleton';
 
 type Props = {
@@ -29,20 +27,11 @@ type SummariseRespose = {
 };
 
 const ReviewList = ({ productId }: Props) => {
-    const {
-        mutate: handleSummarise,
-        isPending: isSummaryLoading,
-        isError: isSummaryError,
-        data: summariseRespose,
-    } = useMutation<SummariseRespose>({
+    const summaryMutation = useMutation<SummariseRespose>({
         mutationFn: () => summariseReviews(),
     });
 
-    const {
-        data: reviewData,
-        isLoading,
-        error,
-    } = useQuery<GetReviewsResponse>({
+    const reviewQuery = useQuery<GetReviewsResponse>({
         queryKey: ['reviews', productId],
         queryFn: () => fetchReviews(),
     }); // don't need useEffect for this bc it is running, caching, retrying etc in the query hook :D
@@ -60,7 +49,7 @@ const ReviewList = ({ productId }: Props) => {
         return data;
     };
 
-    if (isLoading) {
+    if (reviewQuery.isLoading) {
         console.log('is loading');
         return (
             <div className="flex flex-col gap-2">
@@ -71,11 +60,11 @@ const ReviewList = ({ productId }: Props) => {
         );
     }
 
-    if (error) {
+    if (reviewQuery.isError) {
         return <p className="text-red-500">Could not fetch reveiws, please try again.</p>;
     }
 
-    const summary = reviewData?.summary || summariseRespose?.summary;
+    const summary = reviewQuery.data?.summary || summaryMutation.data?.summary;
 
     return (
         <div className="flex flex-col gap-5">
@@ -88,23 +77,23 @@ const ReviewList = ({ productId }: Props) => {
                 <div>
                     <Button
                         className="background-black cursor-pointer"
-                        onClick={() => handleSummarise()}
-                        disabled={isSummaryLoading}
+                        onClick={() => summaryMutation.mutate()}
+                        disabled={summaryMutation.isPending}
                     >
                         <HiSparkles /> Summarise Reviews
                     </Button>
-                    {isSummaryLoading && (
+                    {summaryMutation.isPending && (
                         <div className="py-2">
                             <ReviewSkeleton />
                         </div>
                     )}
-                    {isSummaryError && (
-                        <p className="text-red-500 py-2">Could not summarise error, please try again later!</p>
+                    {summaryMutation.isError && (
+                        <p className="text-red-500 py-2">Could not summarise reviews, please try again later!</p>
                     )}
                 </div>
             )}
-            {reviewData?.reviews &&
-                reviewData.reviews.map((r) => (
+            {reviewQuery.data?.reviews &&
+                reviewQuery.data.reviews.map((r) => (
                     <div key={r.id}>
                         <div className="font-bold">{r.author}</div>
                         <StarRating value={r.rating} />
